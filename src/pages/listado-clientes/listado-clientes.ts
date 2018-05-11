6841779470184bfb01488031b07696626c1d3627
cacerescorem <cacerescorem@gmail.com>
2018-05-11 14:23:32 +0200

    pr

---
+++ src/pages/listado-clientes/listado-clientes.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+
+@IonicPage()
+@Component({
+  selector: 'page-listado-clientes',
+  templateUrl: 'listado-clientes.html',
+})
+export class ListadoClientesPage {
+
+  clientes:any;
+
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              public http: HttpClient,
+              public modal: ModalController) {
+  }
+
+  ionViewDidLoad() {
+    this.http.get('http://localhost:3000/cliente')
+          .map((resp:any)=>{
+            return resp
+          })
+          .subscribe((resp:any)=>{
+            this.clientes = resp.clientes
+          },(error)=>{
+            console.log(error);
+          })
+  }
+
+  crearCliente(){
+    let modal = this.modal.create('CrearClientePage')
+
+    modal.onDidDismiss(cliente=>{
+      if(cliente){
+        this.clientes.push(cliente);
+      }
+    })
+
+    modal.present();
+
+  }
+
+}
